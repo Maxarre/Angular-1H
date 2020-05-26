0240c2be96fbdcffcae761e0359c61269b137b2a
Maxence Poncelet <maxenceponcelet@hotmail.com>
2020-05-26 21:40:44 +0200

    home initial

--- myapp/src/app/home/home.component.ts
+++ myapp/src/app/home/home.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+
+  clickCounter: number = 0;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  countClick() {
+    this.clickCounter += 1;
   }
 
 }
